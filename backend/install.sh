76d2e436b665f4170a2616e4bd0f8169431a4dcc
leohenrique.b@outlook.com <leohenrique.b@outlook.com>
2024-10-27 12:01:06 -0300

    fix:ajuste install

--- backend/install.sh
+++ backend/install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-# Script para iniciar o ngrok apontando para o servidor local na porta 8080
+# Script para iniciar o ngrok apontando para o servidor local na porta 8081
 
-echo "Iniciando ngrok apontando para http://localhost:8080..."
+echo "Iniciando ngrok apontando para http://localhost:8081..."
 
-# Executa o comando ngrok
-ngrok http --url=tight-lark-equal.ngrok-free.app 8087
+# Atualiza o repositório
+git pull
 
-echo "ngrok foi encerrado."
+# Muda para o diretório do projeto e executa o servidor Go em segundo plano
+cd ../../tecnosoft_api/ && go run main.go &
+cd ./cmd/ && go run casamento.go &
+# Muda para o diretório onde o ngrok está localizado
+cd ../../../ngrok-v3-stable-windows-amd64 || { echo "Falha ao mudar de diretório"; exit 1; }
+
+# Executa o ngrok em segundo plano
+./ngrok.exe http --url=tight-lark-equal.ngrok-free.app 8070 &
+
+# echo "ngrok foi iniciado em segundo plano."
