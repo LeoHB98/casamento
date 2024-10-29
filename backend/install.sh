9bd5cdf387192cb14d974970e82d1f968d0566d3
leohenrique.b@outlook.com <leohenrique.b@outlook.com>
2024-10-28 21:51:40 -0300

    feat:ajuste install]

--- backend/install.sh
+++ backend/install.sh
@@ -4,10 +4,10 @@
 echo "Iniciando ngrok apontando para http://localhost:8081..."
 
 # Atualiza o repositório
-git pull
 
 # Muda para o diretório do projeto e executa o servidor Go em segundo plano
-cd ../../tecnosoft_api/ && go run main.go &
+cd ../../tecnosoft_api/ &&  git pull && go run main.go &
+git pull 
 cd ./cmd/ && go run casamento.go &
 # Muda para o diretório onde o ngrok está localizado
 cd ../../../ngrok-v3-stable-windows-amd64 || { echo "Falha ao mudar de diretório"; exit 1; }
