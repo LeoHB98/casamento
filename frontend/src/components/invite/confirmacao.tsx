011a1b47ca1bd9bd1093e94bd3ecf0607af19318
Leonardo <leohenrique.b@outlook.com>
2024-12-12 09:20:09 -0300

    fix:correção envio de codigo para uppercase

--- frontend/src/components/invite/confirmacao.tsx
+++ frontend/src/components/invite/confirmacao.tsx
@@ -1,92 +1,62 @@
-
-import styles from './confirmacao.module.css'
-import { ChangeEvent, FormEvent, useState, } from 'react';
-import { CloseButton } from './closeButton';
+import styles from "./confirmacao.module.css";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { CloseButton } from "./closeButton";
 
 interface ConfirmacaoProps {
-    setState: (value: boolean) => void
-    setCode: (value: string) => void
-    setSendCode: (value: boolean) => void
-    currentCode: string
+  setState: (value: boolean) => void;
+  setCode: (value: string) => void;
+  setSendCode: (value: boolean) => void;
+  currentCode: string;
 }
-
 
 export function Confirmacao(props: ConfirmacaoProps) {
+  const [localCode, setlocalCode] = useState("");
 
-    const [localCode, setlocalCode] = useState('')
+  function handleSetSendCode(event: FormEvent) {
+    event.preventDefault();
 
-    function handleSetSendCode(event: FormEvent) {
-        event.preventDefault()
-
-        if (localCode && localCode.trim().length > 0) {
-            props.setCode(localCode)
-            props.setSendCode(true)
-
-        } else {
-            // Exibe uma mensagem de erro, se necessário
-            // alert('Por favor, preencha o campo com um código válido.');
-
-        }
+    if (localCode && localCode.trim().length > 0) {
+      props.setCode(localCode.toUpperCase());
+      props.setSendCode(true);
+    } else {
+      // Exibe uma mensagem de erro, se necessário
+      // alert('Por favor, preencha o campo com um código válido.');
     }
+  }
 
-    function handleSetNewCode(event: ChangeEvent<HTMLInputElement>) {
+  function handleSetNewCode(event: ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value.trim();
 
-        const value = event.target.value.trim()
-
-        if (value.length === 0) {
-            event.target.setCustomValidity('Valor não pode ser vazio')
-            setlocalCode('')
-        } else {
-            event.target.setCustomValidity('')
-            setlocalCode(event.target.value)
-
-        }
-
+    if (value.length === 0) {
+      event.target.setCustomValidity("Valor não pode ser vazio");
+      setlocalCode("");
+    } else {
+      event.target.setCustomValidity("");
+      setlocalCode(event.target.value);
     }
+  }
 
+  return (
+    <div className={styles.content}>
+      <CloseButton setBoolean={props.setState} setNullString={props.setCode} />
 
-    return (
+      <p>Código disponibilizado pelo(a) noivo(a)</p>
 
+      <form onSubmit={handleSetSendCode} className={styles.inputSpace}>
+        <input
+          name="comment"
+          className={styles.input}
+          onChange={handleSetNewCode}
+          placeholder="Escreva o código aqui..."
+          value={localCode}
+        />
 
-        <div
-            className={styles.content}
-        >
-
-            <CloseButton
-                setBoolean={props.setState}
-                setNullString={props.setCode}
-            />
-
-            <p>
-                Código disponibilizado pelo(a) noivo(a)
-            </p>
-
-            <form
-                onSubmit={handleSetSendCode}
-                className={styles.inputSpace}
-            >
-                <input
-                    name='comment'
-                    className={styles.input}
-                    onChange={handleSetNewCode}
-                    placeholder='Escreva o código aqui...'
-                    value={localCode}
-                />
-
-                <div className={styles.sendButton}>
-                    <button
-                        type='submit'
-                        title='Enviar codigo'
-                    >
-                        Enviar
-                    </button>
-                </div>
-
-            </form>
+        <div className={styles.sendButton}>
+          <button type="submit" title="Enviar codigo">
+            Enviar
+          </button>
         </div>
-
-
-    )
-
+      </form>
+    </div>
+  );
 }
-
